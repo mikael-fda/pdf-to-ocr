9ce5867c61e16fb76427ba94eec467339f43353a
mikael <mikael.almeida@hotmail.fr>
2021-12-11 16:05:08 +0100

    ops: rework docker

--- app/my_count.py
+++ app/my_count.py
@@ -1,6 +1,8 @@
 
 def count_n_w(name, word):
     print(name, word)
-    with open(f"/data/{name}.txt", 'w') as f:
+    with open(f"/data/original/{name}.txt", 'w') as f:
         f.write(name + " - " + word)
     return len(name) + len(word)
+
+
